e53ee7ca7e062f3324aee160b2869d140b30a656
ahmet özkan <kargoozkan@hotmail.com>
2024-05-22 20:25:49 +0300

    001

--- BM104-ProjeOdevi/Controllers/HomeController.cs
+++ BM104-ProjeOdevi/Controllers/HomeController.cs
@@ -18,10 +18,6 @@ namespace BM104_ProjeOdevi.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()

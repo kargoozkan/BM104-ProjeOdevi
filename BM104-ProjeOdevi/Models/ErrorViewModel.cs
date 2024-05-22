e080074111e625f1494a6c85970a2deafb6ecbfe
ahmet özkan <kargoozkan@hotmail.com>
2024-05-22 19:54:41 +0300

    Proje dosyası ekle.

---
+++ BM104-ProjeOdevi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BM104_ProjeOdevi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
